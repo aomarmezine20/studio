9d2b1e3ebe59e3162848713bf8a1e48698f94dcf
aomar mezine <aomarmezine20@gmail.com>
2026-02-28 18:10:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6091907004-453fe",
+  "appId": "1:587564677044:web:01e7fc41e058120cad64e1",
+  "apiKey": "AIzaSyAVE_fLxUBYBn7K5YELQxJtIAHhnuJ74Hc",
+  "authDomain": "studio-6091907004-453fe.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "587564677044"
+};
