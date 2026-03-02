ac131c7a7b9b03e6cfbe1201f35afcf6913d8001
aomar mezine <aomarmezine20@gmail.com>
2026-03-02 02:17:56 +0000

    Set up a Firebase project

--- src/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  "projectId": "studio-6091907004-453fe",
-  "appId": "1:587564677044:web:01e7fc41e058120cad64e1",
-  "apiKey": "AIzaSyAVE_fLxUBYBn7K5YELQxJtIAHhnuJ74Hc",
-  "authDomain": "studio-6091907004-453fe.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "587564677044"
-};
